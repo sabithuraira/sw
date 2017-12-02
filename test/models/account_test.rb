58294f5e373350e2d353af6280b920b553123f3a
Sabit Huraira <sabitzhabit@gmail.com>
2017-12-02 21:55:34 +0700

    fix travis ci & account model testing

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -4,4 +4,25 @@ class AccountTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "email cant be blank" do
+    account = Account.new
+    account.name = "MyString"
+    account.password = "MyString"
+    assert_not account.save, "Saved account without an email"  
+  end
+
+  test "name cant be blank" do
+    account = Account.new
+    account.email = "MyString"
+    account.password = "MyString"
+    assert_not account.save, "Saved account without a name"
+  end
+
+  test "password cant be blank" do
+    account = Account.new
+    account.name = "MyString"
+    account.email = "MyString"
+    assert_not account.save, "Saved account without a password"
+  end
 end
