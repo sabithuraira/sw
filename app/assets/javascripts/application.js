03fe70fcea7d4511496af531513f1246530b7f60
Sabit Huraira <sabitzhabit@gmail.com>
2017-12-04 13:53:08 +0700

    admin bsb theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,22 @@
 //= require react_ujs
 //= require components
 //= require_tree .
+
+//= require plugins/jquery/jquery.min
+//= require plugins/bootstrap/js/bootstrap
+//= require plugins/bootstrap-select/js/bootstrap-select
+//= require plugins/jquery-slimscroll/jquery.slimscroll
+//= require plugins/node-waves/waves
+//= require plugins/jquery-countto/jquery.countTo
+//= require plugins/raphael/raphael.min
+//= require plugins/morrisjs/morris.js
+//= require plugins/chartjs/Chart.bundle
+//= require plugins/flot-charts/jquery.flot
+//= require plugins/flot-charts/jquery.flot.resize
+//= require plugins/flot-charts/jquery.flot.pie
+//= require plugins/flot-charts/jquery.flot.categories
+//= require plugins/flot-charts/jquery.flot.time.js
+//= require plugins/jquery-sparkline/jquery.sparkline.js
+//= require js/admin
+//= require js/pages/index.js
+//= require js/demo.js
